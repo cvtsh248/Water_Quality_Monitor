ffd1ba43774546c6a3e510fac94158f839c0d8fd
Shourjo Chakravarty <cvtsh248@gmail.com>
2019-07-02 13:59:58 +0800

    Communication between arduino and frontend fixed

--- pp1/pp1.ino
+++ pp1/pp1.ino
@@ -1,87 +1,61 @@
-void setup() {
-  Serial.begin(9600);
-  pinMode(2, OUTPUT);
+import controlP5.*;
 
+import processing.serial.*;
+
+Serial arduino;
+
+ControlP5 cp5;
+
+void setup() {
+  size(400,400);
+  
+  arduino = new Serial (this, "/dev/ttyACM0", 9600);
+  arduino.bufferUntil( '\n' );
+  
+  cp5 = new ControlP5(this);
+  cp5.addButton("Data").setValue(0).setPosition(20,20).setSize(90,40);
+  cp5.addButton("Save").setValue(0).setPosition(20,80).setSize(90,40);
 }
 
-float weight = 0;
-float volt = 0; 
-float ppm = 0;
-float avg = 0;
-char incoming; //see when to calibrate
-float offset = 3.43;
-float conversion_factor = 0.67; //if unknown, it is often assumed to be this by authorities
-float gradient = 0.057;
-float micromhos = 0;
-float fine_tune = 5.0;
+float std(float data[]) { //function to calculate standard deviation
+  float avg = 0;
+  float stdev = 0;
+  for (int i = 0; i < data.length; i++) {
+    avg += data[i];
+  }
+  avg /= data.length;
+  for (int i = 0; i < data.length; i++) {
+    stdev += sq(data[i]-avg);
+  }
+  stdev /= data.length;
+  stdev = sqrt(stdev);
+  return (stdev);
+}
 
-void loop() {
-  weight = 0;
-  avg = 0;
-  ppm = 0;
-  micromhos = 0;
-  if (Serial.available()>0){
-    
-    incoming = Serial.read();
-    Serial.println(incoming);
-    
-    if (incoming == 'c'){ //Calibration
-      Serial.println("CALIBRATION MODE, DIP PROBE INTO DISTILLED WATER, YOU HAVE 2 SECONDS TO DO SO");
-      delay(2000);
-      for (int i = 0; i <= 100; i++){
-        digitalWrite(2, HIGH);
-        volt = analogRead(A5);
-        volt = volt*(5.0/1023.0);
-        avg += volt;
-        }
-        digitalWrite(2, LOW);
-        avg = avg/100;
-        offset = avg; //y = mx+c, c is the offset, gradient should remain the same.
-        Serial.println(avg); //print output
-        avg = 0;
-    }
-    
-    if (incoming == 'r'){ //Raw
-      //reading data
-      for (int i = 0; i <= 100; i++){
-        digitalWrite(2, HIGH);
-        volt = analogRead(A5);
-        volt = volt*(5.0/1023.0);
-        avg += volt; //an average is taken for the sake of accuracy, and denoising
-      }
-      digitalWrite(2, LOW);
-      avg = avg/100; 
-      Serial.println(ppm);
-      avg = 0;
-    }
-    
-    if (incoming == 'd'){
-      //reading data
-      for (int i = 0; i <= 100; i++){
-        digitalWrite(2, HIGH);
-        volt = analogRead(A5);
-        volt = volt*(5.0/1023.0);
-        avg += volt; //an average is taken for the sake of accuracy, and denoising
-      }
-      digitalWrite(2, LOW);
-      avg = avg/100;
-      micromhos = 0;
-      micromhos = (avg/gradient)-(offset/gradient)-fine_tune;
-      weight = micromhos;
-      micromhos = micromhos/(100+micromhos);
-      micromhos = micromhos*1000000;
-      micromhos = 1.78*micromhos-169;//conversion
-      if (micromhos < 0){
-        micromhos = 0;
-      }
-      
-      //micromhos = 1.78*(((avg/gradient)-(offset/gradient))/(100+((avg/gradient)-(offset/gradient)))*1000000)-169; //convert voltage to micromhos/microsiemens (literally the same thing)
-      ppm = micromhos*conversion_factor; 
-      Serial.println(avg);
-      //Serial.println(ppm);
-      //Serial.println(weight);
-      ppm = 0;
-      avg = 0;
-    }
+float tds[] = {};
+String val = null;
+
+void draw() {
+  background(0);
+  text("Value tds:",160,45);
+  if (tds.length > 0){
+    text(tds[tds.length-1],250,45);
+  }
+  text("Standard Deviation:",160,70);
+  text(String.format("%.2f", std(tds)),160,90);
+}
+
+public void Data() {
+  arduino.write('d');
+  val = arduino.readStringUntil('\n');
+  //val = arduino.readStringUntil('\n');
+  /*if (Float.isNaN(float(val)) == false){
+    tds = append(tds, float(val));
+  }*/
+}
+
+public void Save() {
+  if (Float.isNaN(float(val)) == false){
+    tds = append(tds, float(val));
   }
 }
